c73043471d26269b52707eb905fcfd41d8fd8cc0
Henry Cañizares <henrycaba471@gamil.con>
2023-08-06 18:15:24 -0500

    commit on render

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-import express  from "express";
+import express from "express";
+import pg from "pg";
+import { config } from "dotenv";
 
+config();
 const app = express();
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+});
 
 app.get('/', (req, res) => {
     res.send("Hola Render")
-})
+});
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()');
+    return res.json(result.rows[0]);
+});
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
